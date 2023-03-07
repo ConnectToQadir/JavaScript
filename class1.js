2b28036fe85d4265752eaf727ef5d82213c82327
connecttoqadir <connectwithqadir@gmail.com>
2023-03-07 19:33:50 +0500

    First Class on JS

---
+++ class1.js
@@ -0,0 +1,51 @@
+// Comments
+// 1. single line  //
+// 2. multi-line /* comment   */
+
+// Variables
+// There are 3 types of variable in JavaScript
+// 1. var
+// 2. let
+// 3. const
+
+// varible naming rules
+// 1. first letter should be an alphabet
+// 2. special charactors are not allowed except underscore _
+// 3. number can be used but not as first charactors
+
+
+// let a // declare
+// a = 234 // initialization
+// const b = 546 // initialization
+
+
+// Differance Between three types of variables
+// const num
+// num = 56
+// const num = 100
+// console.log(num)
+
+// document.write(b)
+// console.log(b)
+
+
+// -------------------------------------------------------------
+    // Learn Tomarrow Insha-Allah
+// -------------------------------------------------------------
+
+// Operators
+// 1. Assignment     =>    =
+// 2. Arithematic    =>    + , - , * , / , % , ++ , -- , += , -= , *= , /=
+// 3. Comparision    =>    > , => , < , <= , == , === , != , !==
+// 4. Logical        =>    && , || , !
+
+// Data Types
+// 1. String
+// 2. Number
+// 3. Array
+// 4. Object
+// 5. null
+// 6. undefined
+// 7. Boolean
+
+
