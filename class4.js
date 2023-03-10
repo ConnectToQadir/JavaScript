dbff38e7851d942dd36fd4bb6119d429b1ca7428
ConnectToQadir <connectwithqadir@gmail.com>
2023-03-10 19:53:49 +0500

    4th class on js

---
+++ class4.js
@@ -0,0 +1,89 @@
+
+
+// var dayNumber = prompt("Enter the week Day :")
+
+// switch (dayNumber) {
+//     case "1":
+//         document.write("Sunday")
+//         break
+//     case "2":
+//         document.write("Monday")
+//         break
+//     case "3":
+//         document.write("Tuesday")
+//         break
+//     case "4":
+//         document.write("Wednesday")
+//         break
+//     case "5":
+//         document.write("Thursday")
+//         break
+//     case "6":
+//         document.write("Friday")
+//         break
+//     case "7":
+//         document.write("Saturday")
+//         break
+//     default:
+//         document.write("Enter a valid day")
+// }
+
+// if(2>1){
+//     document.write("2 is greater")
+// }else{
+//     document.write("2 is smaller")
+// }
+
+// Replacement to if else is conditional which is defined below:
+// 2>5?document.write("2 is greater"):document.write("2 is smaller")
+
+
+// Itrational Control Stucture ( Loops )
+// for, while, do while
+
+
+// for( var i = 1  ;  i <= 10  ; i++  ){
+//     document.write(i," . Hello World <br>")
+// }
+
+// var i = 1
+// while(i <= 99){
+//     document.write(i,"<br>")
+//     i += 2
+// }
+
+// var i = 1
+// do {
+//     document.write(i, "<br>")
+//     i++
+// } while (i <= 0)
+
+// Data Types
+// 1. "String"
+// 2. Number
+// 3. [Array]
+// 4. {Object}
+// 5. null
+// 6. undefined
+// 7. Boolean
+
+// var str = "jo marzi likh lo edher"
+// var num = 4323.323
+// var loading = false
+// var names = ["Ashar","Abdul Qadir","Muhib","Zain","Bilal"]
+// var data = {name:"Ashar",age:20,edu:"BSCS"}
+
+var data
+// JSON.stringify() => used to convert object to JSON string
+document.write(data ," => " , typeof(data))
+console.log(data)
+
+// typeOf() is fuction to check data type of any data or variable
+
+
+
+
+
+
+
+
